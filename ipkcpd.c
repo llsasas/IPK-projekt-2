dd5e28a079890109e0857e0e066b996d3ba519a2
Ilsasas <cus.samuel@gmail.com>
2023-04-15 11:40:08 +0200

    Add arguments_check function for command line argument validation
    This commit adds the 'arguments_check function' to the project. This function takes command line arguments and validates them according to the requirements of the project. It returns 0 if the arguments are valid, and 1 otherwise. Additionally, a header comment has been added to the ipkcpcd.c file, providing details about the file's purpose, author, brief description, and creation date.

--- ipkcpd.c
+++ ipkcpd.c
@@ -1,4 +1,76 @@
+/**
+ * @file ipkcpd.c
+ * @author Samuel Cus (xcussa00@fit.vutbr.cz)
+ * @brief  IPK Project 2
+ * @date 2023-04-15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int arguments_check(int argn, const char *arga[], int *h, int *p, int *m)
+{
+    bool swp = false;
+    bool swh = false;
+    bool swm = false;
+    if (argn != 7)
+    {
+        return 1;
+    }
+    for (int i = 1; i < 7; i++)
+    {
+        if (strcmp(arga[i], "-h") == 0)
+        {
+            *h = i + 1;
+            if (swm == true && swp == true)
+            {
+                return 0;
+            }
+            swh = true;
+            continue;
+        }
+        if (strcmp(arga[i], "-p") == 0)
+        {
+            *p = i + 1;
+            if (swh == true && swm == true)
+            {
+                return 0;
+            }
+            else
+            {
+                swp = true;
+                continue;
+            }
+        }
+        if (strcmp(arga[i], "-m") == 0)
+        {
+            if (strcmp(arga[i + 1], "tcp") == 0 || strcmp(arga[i + 1], "udp") == 0)
+            {
+                *m = i + 1;
+                swm = true;
+                if (swh == true && swp == true)
+                    return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+    if (swh != true || swp != true || swm != true)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main(int argc, const char *argv[])
 {
+    
     return 0;
 }
